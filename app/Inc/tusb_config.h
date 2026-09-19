55bd4e8ca47a2694ed011a736bc3ba0a22151b6a
slemanz <william_sleman@hotmail.com>
2026-09-19 19:19:16 -0300

    app(feat): add tusb config

---
+++ app/Inc/tusb_config.h
@@ -0,0 +1,19 @@
+#ifndef TUSB_CONFIG_H_
+#define TUSB_CONFIG_H_
+
+// defined by compiler flags for flexibility
+#ifndef CFG_TUSB_MCU
+#error CFG_TUSB_MCU must be defined
+#endif
+
+#define CFG_TUSB_OS           OPT_OS_NONE
+#define CFG_TUSB_DEBUG        0
+
+#define CFG_TUD_ENABLED       1
+#define BOARD_TUD_MAX_SPEED   OPT_MODE_DEFAULT_SPEED
+
+#define CFG_TUD_CDC              1
+#define CFG_TUD_CDC_NOTIFY       1
+
+
+#endif /* TUSB_CONFIG_H_ */
