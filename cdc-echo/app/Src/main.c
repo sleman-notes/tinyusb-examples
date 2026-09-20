0059f6453ffa53496802fd763bdfcd9657e264fb
slemanz <william_sleman@hotmail.com>
2026-09-20 18:38:56 -0300

    cdc-echo(feat): add printf retarget

--- cdc-echo/app/Src/main.c
+++ cdc-echo/app/Src/main.c
@@ -1,14 +1,26 @@
 #include "config.h"
 #include "usb.h"
 #include "tusb.h"
+#include <stdio.h>
+
+static void usb_wait_connection(void)
+{
+    while(!tud_cdc_connected())
+    {
+        tud_task();
+    }
+}
 
 int main(void)
 {
     config_app();
 	usb_init();
+	usb_wait_connection();
 
 	ticks_timeout_t blink;
 	ticks_timeoutInit(&blink, BLINK_PERIOD_MS);
+
+	printf("Init the board!\r\n");
 
 	while(1)
 	{
@@ -29,4 +41,13 @@ int main(void)
 			tud_cdc_write_flush();
 		}
 	}
+}
+
+int __io_putchar(int ch)
+{
+	tud_cdc_write_char((char)ch);
+
+    if(ch == '\n') tud_cdc_write_flush();
+
+    return ch;
 }
